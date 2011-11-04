af879ffa08e9197489cb4afa1b465fc4e67871fa
John Mair <jrmair@gmail.com>
2011-11-05 03:47:50 +1300

    added frame and frame-type commands to ease navigation, etc

--- lib/pry_time/commands.rb
+++ lib/pry_time/commands.rb
@@ -4,14 +4,21 @@ PryTime::PryTimeCommands = Pry::CommandSet.new do
     inc = inc_str.nil? ? 1 : inc_str.to_i
 
     PryTime.data[:instance].binding_index += inc
-    _pry_.run_command "whereami"
   end
 
   command "down", "Go down to the callee's context" do |inc_str|
     inc = inc_str.nil? ? 1 : inc_str.to_i
 
     PryTime.data[:instance].binding_index -= inc
-    _pry_.run_command "whereami"
+  end
+
+  command "frame", "Switch to a particular frame" do |frame_num|
+    PryTime.data[:instance].binding_index = frame_num.to_i
+  end
+
+  command "frame-type", "Display current frame type." do
+    bindex = PryTime.data[:instance].binding_index
+    output.puts PryTime.data[:instance].exception_bindings[bindex].frame_type
   end
 
   command "bt", "Show the full backtrace for the current exception" do
@@ -24,19 +31,6 @@ PryTime::PryTimeCommands = Pry::CommandSet.new do
       _pry_.run_command "exit-all :__continue__"
     else
       PryTime.data[:instance].current_exception.continue
-    end
-  end
-
-  helpers do
-    def update_current_binding(current_binding)
-      if current_binding
-        output.puts "Can't locate source for eval'd code" if current_binding.eval("__FILE__") == "(eval)"
-
-        PryTime.data[:instance].binding_stack[-1] = current_binding
-        PryTime.data[:instance].run_command "whereami"
-      else
-        output.puts "Reached end of stacktrace, go back down the stack by typing down"
-      end
     end
   end
 end
